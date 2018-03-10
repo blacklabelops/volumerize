52e315cdb58ffe18d5a2a176d67059f9208fc825
Steffen Bleul <sbl@blacklabelops.com>
2018-03-10 16:15:10 +0100

    Removing postfix for development version.

--- buildscripts/buildImage.sh
+++ buildscripts/buildImage.sh
@@ -6,11 +6,6 @@ function buildImage() {
   local tagname=$1
   local version=$2
   local branch=$BUILD_BRANCH
-  if  [ "${branch}" = "master" ]; then
-    imagename=$tagname
-  else
-    imagename=$tagname-development
-  fi
   docker build --no-cache -t blacklabelops/volumerize:$imagename .
 }
 
