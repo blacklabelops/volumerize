4da5f4cfa7c2e151530b5b04a91c268f918f0e7c
Steffen Bleul <sbl@blacklabelops.com>
2018-07-10 18:57:06 +0200

    New image version.

--- buildscripts/release.sh
+++ buildscripts/release.sh
@@ -3,5 +3,5 @@
 #------------------
 # CONTAINER VARIABLES
 #------------------
-export IMAGE_VERSION=1.1.0
+export IMAGE_VERSION=1.2.0
 export BUILD_BRANCH=$(git branch | grep -e "^*" | cut -d' ' -f 2)
