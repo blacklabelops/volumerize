c45c402660c535cee505a2d3a363fe2e034c46a9
Steffen Bleul <sbl@itbleul.de>
2018-10-06 12:45:34 +0200

    Switching to circleci build system.

--- buildscripts/alpineTestImages.groovy
+++
@@ -1,9 +0,0 @@
-node('docker') {
-    checkout scm
-    sh './buildscripts/release.sh && ./buildscripts/cleanAlpineContainers.sh'
-    try {
-      sh './buildscripts/release.sh && ./buildscripts/testSupportedAlpineImages.sh'
-    } finally {
-      sh './buildscripts/release.sh && ./buildscripts/cleanAlpineContainers.sh'
-    }
-}
