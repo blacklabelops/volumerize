db4e1821483e147bf7451b6a08d2a4cc70852796
Anton <antonymousfeedback@gmail.com>
2019-08-29 13:22:54 +0200

    Put $@ in double quotes everywhere

--- prepost_strategies/mongodb/preexecute/utils/check-env.sh
+++ prepost_strategies/mongodb/preexecute/utils/check-env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-## 
+##
  # @private
  # Executed if a variable is not set or is empty string
  # @param1 - Name of the failed environment variable
@@ -11,7 +11,7 @@ function _check_env_failed {
     exit 1
 }
 
-## 
+##
  # @private
  # Executed if a variable is setted
  # @param1 - Name of the environment variable
@@ -20,7 +20,7 @@ function _check_env_ok {
     echo "Env var $1 ok."
 }
 
-## 
+##
  # Use it to check if environment variables are set
  # @param1      - Name of the context
  # @param2 to ∞ - Environment variables to check
@@ -28,14 +28,14 @@ function _check_env_ok {
 function check_env {
     echo "Checking environment variables for $1."
 
-    for e_var in "$@"; do
+    for e_var in ""$@""; do
         if [ $e_var = $1 ]; then continue; fi # Jump first arg
-        
+
         # Check if env var is setted, if not raise error
-        if [ "${!e_var}" = "" ]; then 
-            _check_env_failed $e_var; 
-        else 
-            _check_env_ok $e_var; 
+        if [ "${!e_var}" = "" ]; then
+            _check_env_failed $e_var;
+        else
+            _check_env_ok $e_var;
         fi
 
     done
